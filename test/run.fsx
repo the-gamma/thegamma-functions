e879aacca33058039f2cc7023271a30df8502759
Tomas Petricek <tomas@tomasp.net>
2017-06-18 02:40:54 +0100

    Testing things

--- test/run.fsx
+++ test/run.fsx
@@ -1,18 +1,43 @@
+#if !COMPILED
+#r "../packages/Microsoft.Azure.WebJobs/lib/net45/Microsoft.Azure.WebJobs.Host.dll"
+#r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+#r "../packages/Microsoft.AspNet.WebApi.Client/lib/net45/System.Net.Http.Formatting.dll"
+#r "../packages/Microsoft.AspNet.WebApi.Core/lib/net45/System.Web.Http.dll"
 #r "System.Net.Http"
 #r "Newtonsoft.Json"
+#else
+module MyAzureFunction
+#endif
+
+#r @"..\packages\Suave\lib\net40\Suave.dll"
 
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
+open Microsoft.Azure.WebJobs.Host
+open System
 
-type Named = {
-    name: string
+type Message = {
+    name : string
 }
+
+open Suave.Http
 
 let Run(req: HttpRequestMessage, log: TraceWriter) =
     async {
         log.Info(sprintf 
             "F# HTTP trigger function processed a request.")
+
+        let ctx = 
+          let req = Unchecked.defaultof<HttpRequest>
+          let res = Unchecked.defaultof<HttpResult>
+          let rtm = Unchecked.defaultof<HttpRuntime>
+          let cnn = Unchecked.defaultof<_>
+          { request = req
+            runtime = rtm
+            connection = cnn 
+            userState = Map.empty
+            response = res }
 
         // Set name to query string
         let name =
@@ -26,7 +51,7 @@ let Run(req: HttpRequestMessage, log: TraceWriter) =
             let! data = req.Content.ReadAsStringAsync() |> Async.AwaitTask
 
             if not (String.IsNullOrEmpty(data)) then
-                let named = JsonConvert.DeserializeObject<Named>(data)
+                let named = JsonConvert.DeserializeObject<Message>(data)
                 return req.CreateResponse(HttpStatusCode.OK, "Hello " + named.name);
             else
                 return req.CreateResponse(HttpStatusCode.BadRequest, "Specify a Name value");
