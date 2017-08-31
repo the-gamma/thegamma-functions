7c5adf030edc7195e8ec1b50b98bd5f0a37fd44a
Tomas Petricek <tomas@tomasp.net>
2017-08-31 14:42:44 +0100

    Test function generation

--- build.fsx
+++ build.fsx
@@ -1,26 +1,48 @@
+#r "packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+let code = """
 #r "../packages/Microsoft.Azure.WebJobs/lib/net45/Microsoft.Azure.WebJobs.Host.dll"
-//#r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 #r "../packages/Microsoft.AspNet.WebApi.Client/lib/net45/System.Net.Http.Formatting.dll"
 #r "../packages/Microsoft.AspNet.WebApi.Core/lib/net45/System.Web.Http.dll"
-//#r "../packages/Suave/lib/net40/Suave.dll"
 #r "System.Net.Http"
-//#r "Newtonsoft.Json"
 
-//#load "../helper.fs"
 open System.Net
 open System.Net.Http
-//open Newtonsoft.Json
 open Microsoft.Azure.WebJobs.Host
-//open System
-(*
-type Message = {
-    name : string
-}
 
-open Suave.Http
-*)
 let Run(req: HttpRequestMessage, log: TraceWriter) =
   req.CreateResponse(HttpStatusCode.OK, "Totally")
+"""
+
+let config = """
+{
+  "bindings": [
+    {
+      "authLevel": "anonymous",
+      "name": "req",
+      "type": "httpTrigger",
+      "direction": "in"
+    },
+    {
+      "name": "res",
+      "type": "http",
+      "direction": "out"
+    }
+  ],
+  "disabled": false
+}"""
+
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+Target "generate" (fun _ ->  
+  for func in ["experiment"] do
+    CleanDir func
+    WriteStringToFile false (func </> "run.fsx") code
+    WriteStringToFile false (func </> "function.json") config
+)
+
+RunTargetOrDefault "generate"
 
 (*
     async {
