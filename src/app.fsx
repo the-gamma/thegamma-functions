8f436f8250b6a41cc192c2fe54ef2ec4562c4611
Tomas Petricek <tomas@tomasp.net>
2017-08-31 19:15:11 +0100

    Test deploy

--- src/app.fsx
+++ src/app.fsx
@@ -2,11 +2,17 @@
 open Lambda
 
 let app = 
-  [ func "demo" HttpInput HttpOutput (fun input ->
+  [ func "first" HttpInput HttpOutput (fun input ->
       let name = defaultArg (Map.tryFind "name" input.Query) "anonymous"
       { Status = 200
         Encoding = System.Text.Encoding.UTF8
         Content = "Hello " + name
+        Headers = Map.empty
+        ContentType = "text/plain" })
+    func "second" HttpInput HttpOutput (fun input ->
+      { Status = 200
+        Encoding = System.Text.Encoding.UTF8
+        Content = "Nothing useful"
         Headers = Map.empty
         ContentType = "text/plain" })
   ]
