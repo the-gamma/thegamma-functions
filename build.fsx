8d6988b25d2ea705f1abcf465762f1bbd503167a
Tomas Petricek <tomas@tomasp.net>
2017-08-31 19:27:42 +0100

    Fix direcotry handling 2

--- build.fsx
+++ build.fsx
@@ -30,7 +30,7 @@ let config = """
   "disabled": false
 }"""
 
-let root = __SOURCE_DIRECTORY__ </> "../wwwroot"
+let root = __SOURCE_DIRECTORY__ </> "/../wwwroot"
 
 Target "generate" (fun _ ->  
   let special = set ["src"; ".fake"; ".git"; ".paket"; "packages"]
